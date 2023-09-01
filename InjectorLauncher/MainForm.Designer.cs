8d81f46fac84bec541ba178b244b9d169e39a820
HexrtlessAngel <95123544+HexrtlessAngel@users.noreply.github.com>
2023-09-01 08:49:49 -0400

    Update MainForm.Designer.cs

--- InjectorLauncher/MainForm.Designer.cs
+++ InjectorLauncher/MainForm.Designer.cs
@@ -82,6 +82,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HeartlessAngel\'s DLL Injector";
             this.ResumeLayout(false);
             this.PerformLayout();
